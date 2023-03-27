a51aae937690bf62afd7e51d2964ea36320bdd0a
Manuel Gago <manuelmeg96@gmail.com>
2023-03-27 15:15:34 +0000

    preparing for burguer menu

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", function () {
+    let burguer_menu_display = document.querySelector(".burguer-menu-display");
+    let burger_menu_img = document.querySelector("burguer-menu-img");
+    // this is for darkening the 'body' when 
+    let main = document.querySelector("body")
+
+
+});
+/*
+
+    if(localStorage.carrito){
+        let carrito1 = JSON.parse(localStorage.carrito)
+        carrito1.forEach(function(product){
+            contador += product.quantity 
+        })
+        p.innerHTML = contador
+
+    }
+    else{
+        p.innerHTML = contador
+    }
+
+
+    burger_menu_button.addEventListener("click", function(){
+        burger_menu.classList.remove('burger-menu-display-none')
+        main.addEventListener("click", function(){
+            burger_menu.classList.add('burger-menu-display-none')
+        })
+
+    })
+    
+})
+
+*/
