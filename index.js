9dcca6014cdb52be345e46845ec7d63022aede1e
Gago Manuel <manuel.gago@arba.gov.ar>
2023-07-14 11:06:55 -0300

    nav-bar running good

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
 window.addEventListener("load", function () {
     let burguer_menu_display = document.querySelector(".burguer-menu-display");
     let burguer_menu_column = document.querySelector(".burguer-menu-column");
-    let burger_menu_img = document.querySelector("burguer-menu-img");
+    let burger_menu_img = document.querySelector(".burguer-menu-img.menu");
+    let burger_menu_close = document.querySelector(".nav-x-to-close");
     // this is for darkening the 'body' when 
     let body = document.querySelector("body");
 
-
-});
-/*
-
-    if(localStorage.carrito){
-        let carrito1 = JSON.parse(localStorage.carrito)
-        carrito1.forEach(function(product){
-            contador += product.quantity 
-        })
-        p.innerHTML = contador
-
-    }
-    else{
-        p.innerHTML = contador
-    }
-
-
-    burger_menu_button.addEventListener("click", function(){
-        burger_menu.classList.remove('burger-menu-display-none')
-        main.addEventListener("click", function(){
-            burger_menu.classList.add('burger-menu-display-none')
-        })
-
+    burger_menu_img.addEventListener("click", function(){
+        if (burguer_menu_display.classList.contains('toggle-view')){
+            burguer_menu_display.classList.remove("toggle-view")
+        }
     })
-    
-})
-
-*/
+    burger_menu_close.addEventListener("click", function(){
+        if (!burguer_menu_display.classList.contains('toggle-view')){
+            burguer_menu_display.classList.add("toggle-view")
+        }
+    })
+});
